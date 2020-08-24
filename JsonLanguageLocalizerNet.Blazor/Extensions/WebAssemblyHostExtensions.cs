4d05666d087aa6ad1d8890836f27fe293adbfe84
Anthony Rivera <arivera@amcgroup.pr>
2020-08-24 18:54:58 -0400

    Removed JsonLanguageLocalizerServiceHelper and JsonLanguageLocalizerNet.Blazor.Helpers namespace.

--- JsonLanguageLocalizerNet.Blazor/Extensions/WebAssemblyHostExtensions.cs
+++ JsonLanguageLocalizerNet.Blazor/Extensions/WebAssemblyHostExtensions.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.JSInterop;
 using System;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace JsonLanguageLocalizerNet.Blazor
@@ -32,12 +35,12 @@ namespace JsonLanguageLocalizerNet.Blazor
                 throw new ArgumentException($"The fallbackCulture value ({fallbackCulture}) could not be found the supported cultures. The fallbackCulture value must be included in the suppoted cultures.");
             }
             //We try to lookup the locale in the browser storage
-            var jS = webAssemblyHost.Services.GetRequiredService<IJSRuntime>();
-            var applicationLocale = await jS.InvokeAsync<string>("eval", $"window.localStorage.getItem('{localStorageKey}')");
+            var jsRuntime = webAssemblyHost.Services.GetRequiredService<IJSRuntime>();
+            var applicationLocale = await jsRuntime.InvokeAsync<string>("eval", $"window.localStorage.getItem('{localStorageKey}')");
             if (string.IsNullOrWhiteSpace(applicationLocale))
             {
                 //We try use the browser navigator language
-                await webAssemblyHost.TryInitializeJsonLanguageLocalizerSupportedCulturesServiceFromNavigatorLanguageAsync(fallbackCulture);
+                await webAssemblyHost.TryInitializeJsonLanguageLocalizerSupportedCulturesServiceFromNavigatorLanguageAsync(fallbackCulture, localStorageKey);
             }
             else
             {
@@ -45,16 +48,16 @@ namespace JsonLanguageLocalizerNet.Blazor
                 if (supportedCultures.GetLanguageLocalizerSupportedCultures().SupportedCultures.Any(w => w.Name == applicationLocale))
                 {
                     //We use the browser storage locale since it's supported
-                    InitializeDefaultThreadCurrentCultureAndUI(applicationLocale);
+                    await webAssemblyHost.InitializeDefaultThreadCurrentCultureAndUIAsync(applicationLocale, localStorageKey);
                 }
                 else
                 {
                     //We try use the browser navigator language
-                    await webAssemblyHost.TryInitializeJsonLanguageLocalizerSupportedCulturesServiceFromNavigatorLanguageAsync(fallbackCulture);
+                    await webAssemblyHost.TryInitializeJsonLanguageLocalizerSupportedCulturesServiceFromNavigatorLanguageAsync(fallbackCulture, localStorageKey);
                 }
             }
         }
-        private static async Task TryInitializeJsonLanguageLocalizerSupportedCulturesServiceFromNavigatorLanguageAsync(this WebAssemblyHost webAssemblyHost, string fallbackCulture)
+        private static async Task TryInitializeJsonLanguageLocalizerSupportedCulturesServiceFromNavigatorLanguageAsync(this WebAssemblyHost webAssemblyHost, string fallbackCulture, string localStorageKey)
         {
             var supportedCultures = webAssemblyHost.Services.GetRequiredService<IJsonLanguageLocalizerSupportedCulturesService>();
             //We try to lookup the locale from the navigator language
@@ -64,7 +67,7 @@ namespace JsonLanguageLocalizerNet.Blazor
             if (string.IsNullOrWhiteSpace(navigatorLanguage))
             {
                 //The navigator language is not supported then we use the fallback culture
-                InitializeDefaultThreadCurrentCultureAndUI(fallbackCulture);
+                await webAssemblyHost.InitializeDefaultThreadCurrentCultureAndUIAsync(fallbackCulture, localStorageKey);
             }
             else
             {
@@ -72,20 +75,104 @@ namespace JsonLanguageLocalizerNet.Blazor
                 if (supportedCultures.GetLanguageLocalizerSupportedCultures().SupportedCultures.Any(w => w.Name == navigatorLanguage))
                 {
                     //We use the navigator language culture since it's supported
-                    InitializeDefaultThreadCurrentCultureAndUI(navigatorLanguage);
+                    await webAssemblyHost.InitializeDefaultThreadCurrentCultureAndUIAsync(navigatorLanguage, localStorageKey);
                 }
                 else
                 {
                     //The navigator language is not supported then we use the fallback culture
-                    InitializeDefaultThreadCurrentCultureAndUI(fallbackCulture);
+                    await webAssemblyHost.InitializeDefaultThreadCurrentCultureAndUIAsync(fallbackCulture, localStorageKey);
                 }
             }
         }
-        private static void InitializeDefaultThreadCurrentCultureAndUI(string name)
+        private static async Task InitializeDefaultThreadCurrentCultureAndUIAsync(this WebAssemblyHost webAssemblyHost, string name, string localStorageKey)
         {
+            var jsRuntime = webAssemblyHost.Services.GetRequiredService<IJSRuntime>();
+            await jsRuntime.InvokeAsync<string>("eval", $"window.localStorage.setItem('{localStorageKey}', '{name}')");
             var cultureInfo = new CultureInfo(name);
             CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
             CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        }
+        /// <summary>
+        /// Loads the json file into the service using the supported cultures service within the desired strategy and loading preference. 
+        /// </summary>
+        /// <param name="webAssemblyHost"></param>
+        /// <returns></returns>
+        public static async Task<JsonLanguageLocalizerService> GetJsonLanguageLocalizerServiceFromSupportedCulturesAsync(this WebAssemblyHost webAssemblyHost, HttpClient httpClient, LanguageLocalizerSupportedCultures languageLocalizerSupportedCultures, string localStorageKey = "ApplicationLocale")
+        {
+            if (!languageLocalizerSupportedCultures.SupportedCultures.Any())
+            {
+                throw new Exception("There is no supported cultures defined. Make sure there is at least one supported language.");
+            }
+            JsonLanguageLocalizerService result = null;
+            var jsRuntime = webAssemblyHost.Services.GetRequiredService<IJSRuntime>();
+            var applicationLocale = await jsRuntime.InvokeAsync<string>("eval", $"window.localStorage.getItem('{localStorageKey}')");
+            var languageLocalizerSupportedCultureSelected = languageLocalizerSupportedCultures.SupportedCultures.FirstOrDefault(w => w.Name == applicationLocale);
+            if (languageLocalizerSupportedCultureSelected == null)
+            {
+                languageLocalizerSupportedCultureSelected = languageLocalizerSupportedCultures.SupportedCultures.FirstOrDefault(w => w.CultureInfo.Equals(new CultureInfo(languageLocalizerSupportedCultures.FallbackCulture)));
+            }
+            if (languageLocalizerSupportedCultures.UseRemoteSourceAlwaysWhenAvailable)
+            {
+                result = await TryGetRemoteSourceAsync(httpClient, languageLocalizerSupportedCultures.HttpMethod, languageLocalizerSupportedCultureSelected.RemoteSource, languageLocalizerSupportedCultures.RemoteRetryTimes);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+            if (result == null && !languageLocalizerSupportedCultures.UseLocalSourceWhenRemoteSourceFails)
+            {
+                throw new Exception("We could not load the remote source of Language Localizer");
+            }
+            if (result == null && languageLocalizerSupportedCultures.UseLocalSourceWhenRemoteSourceFails)
+            {
+                if (languageLocalizerSupportedCultures.LocalSourceStrategy == SourceStrategy.FileSystem)
+                {
+                    result = TryGetLocalSourceFromFileSystem(languageLocalizerSupportedCultureSelected.LocalSource);
+                }
+                else if (languageLocalizerSupportedCultures.LocalSourceStrategy == SourceStrategy.HttpRequest)
+                {
+                    result = await TryGetRemoteSourceAsync(httpClient, languageLocalizerSupportedCultures.HttpMethod, languageLocalizerSupportedCultureSelected.LocalSource, languageLocalizerSupportedCultures.RemoteRetryTimes);
+                }
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+            if (result == null)
+            {
+                throw new Exception("We could not load neither remote or local source of Language Localizer");
+            }
+            return null;
+        }
+        private static async Task<JsonLanguageLocalizerService> TryGetRemoteSourceAsync(HttpClient httpClient, string httpMethod, string requestUri, int remoteRetryTimes)
+        {
+            do
+            {
+                try
+                {
+                    var request = new HttpRequestMessage(new HttpMethod(httpMethod), requestUri);
+                    request.SetBrowserRequestCache(BrowserRequestCache.NoCache);
+                    var response = await httpClient.SendAsync(request);
+                    return new JsonLanguageLocalizerService(await response.Content.ReadAsStreamAsync());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (--remoteRetryTimes > 0);
+            return null;
+        }
+        private static JsonLanguageLocalizerService TryGetLocalSourceFromFileSystem(string path)
+        {
+            try
+            {
+                return new JsonLanguageLocalizerService(File.OpenRead(path));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
         }
     }
 }
