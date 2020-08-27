58e7982f7baabd291c0162e7c1142118392cdeb3
Anthony Rivera <arivera@amcgroup.pr>
2020-08-27 12:11:30 -0400

    Refactor to make it compatible with blazor and blazmobile with standalone implementation. Removed WebAssemblyHostExtensions.

--- JsonLanguageLocalizerNet.Blazor/Helpers/JsonLanguageLocalizerServiceBlazorHelper.cs
+++ JsonLanguageLocalizerNet.Blazor/Helpers/JsonLanguageLocalizerServiceBlazorHelper.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Http;
+﻿using JsonLanguageLocalizerNet.Helpers;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
 using System;
+using System.Globalization;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -7,6 +10,58 @@ namespace JsonLanguageLocalizerNet.Blazor.Helpers
 {
     public static class JsonLanguageLocalizerServiceBlazorHelper
     {
+        /// <summary>
+        /// Loads the json file into the service using the supported cultures service within the desired strategy and loading preference. 
+        /// </summary>
+        /// <param name="applicationLocale"></param>
+        /// <param name="httpClient"></param>
+        /// <param name="languageLocalizerSupportedCultures"></param>
+        /// <returns></returns>
+        public static async Task<JsonLanguageLocalizerService> GetJsonLanguageLocalizerServiceFromSupportedCulturesAsync(HttpClient httpClient, LanguageLocalizerSupportedCultures languageLocalizerSupportedCultures, string applicationLocale)
+        {
+            if (!languageLocalizerSupportedCultures.SupportedCultures.Any())
+            {
+                throw new Exception("There is no supported cultures defined. Make sure there is at least one supported language.");
+            }
+            JsonLanguageLocalizerService result = null;
+            var languageLocalizerSupportedCultureSelected = languageLocalizerSupportedCultures.SupportedCultures.FirstOrDefault(w => w.Name == applicationLocale);
+            if (languageLocalizerSupportedCultureSelected == null)
+            {
+                languageLocalizerSupportedCultureSelected = languageLocalizerSupportedCultures.SupportedCultures.FirstOrDefault(w => w.CultureInfo.Equals(new CultureInfo(languageLocalizerSupportedCultures.FallbackCulture)));
+            }
+            if (languageLocalizerSupportedCultures.UseRemoteSourceAlwaysWhenAvailable)
+            {
+                result = await TryGetRemoteSourceAsync(httpClient, languageLocalizerSupportedCultures.HttpMethod, languageLocalizerSupportedCultureSelected.RemoteSource, languageLocalizerSupportedCultures.RemoteRetryTimes);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+            if (result == null && !languageLocalizerSupportedCultures.UseLocalSourceWhenRemoteSourceFails)
+            {
+                throw new Exception("We could not load the remote source of Language Localizer");
+            }
+            if (result == null && languageLocalizerSupportedCultures.UseLocalSourceWhenRemoteSourceFails)
+            {
+                if (languageLocalizerSupportedCultures.LocalSourceStrategy == SourceStrategy.FileSystem)
+                {
+                    result = JsonLanguageLocalizerServiceHelper.TryGetLocalSourceFromFileSystem(languageLocalizerSupportedCultureSelected.LocalSource);
+                }
+                else if (languageLocalizerSupportedCultures.LocalSourceStrategy == SourceStrategy.HttpRequest)
+                {
+                    result = await JsonLanguageLocalizerServiceHelper.TryGetRemoteSourceAsync(httpClient, languageLocalizerSupportedCultures.HttpMethod, languageLocalizerSupportedCultureSelected.LocalSource, languageLocalizerSupportedCultures.RemoteRetryTimes);
+                }
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+            if (result == null)
+            {
+                throw new Exception("We could not load neither remote or local source of Language Localizer");
+            }
+            return null;
+        }
         /// <summary>
         /// The main difference between this helper method and the one on JsonLanguageLocalizerServiceHelper is that this method prevent caching by the browser when requesting static files.
         /// </summary>
